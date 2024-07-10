ec1044a636f0474559e929f1ad738c8388e0c835
Tim Heuer <tim@timheuer.com>
2024-07-10 13:35:21 -0700

    add classlib

---
+++ PrimeLibrary/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace PrimeLibrary;
+
+public class Class1
+{
+    public bool IsPrime(int candidate)
+    {
+        if (candidate < 2)
+        {
+            return false;
+        }
+
+        for (int divisor = 2; divisor <= Math.Sqrt(candidate); ++divisor)
+        {
+            if (candidate % divisor == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -5,10 +5,12 @@ namespace TestProject1;
 public class UnitTest1
 {
     private ITestOutputHelper _output;
+    readonly PrimeLibrary.Class1 _primeService;
 
     public UnitTest1(ITestOutputHelper output)
     {
         _output = output;
+        _primeService = new PrimeLibrary.Class1();
     }
 
     [Fact]
@@ -24,4 +26,19 @@ public class UnitTest1
         _output.WriteLine("This xUnit test passed");
         Assert.True(true);
     }
+
+    [Theory]
+    [InlineData(-1), InlineData(0), InlineData(1)]
+    public void IsPrime_ValuesLessThan2_ReturnFalse(int value) =>
+        Assert.False(_primeService.IsPrime(value), $"{value} should not be prime");
+
+    [Theory]
+    [InlineData(2), InlineData(3), InlineData(5), InlineData(7)]
+    public void IsPrime_PrimesLessThan10_ReturnTrue(int value) =>
+        Assert.True(_primeService.IsPrime(value), $"{value} should be prime");
+
+    [Theory]
+    [InlineData(4), InlineData(6), InlineData(8), InlineData(9)]
+    public void IsPrime_NonPrimesLessThan10_ReturnFalse(int value) =>
+        Assert.False(_primeService.IsPrime(value), $"{value} should not be prime");
 }
